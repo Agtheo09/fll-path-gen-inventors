2fbd44358dc9ce1463e4d230e50759425a71ebf5
Angelos Theodoridis <72686781+Agtheo09@users.noreply.github.com>
2024-04-28 23:23:45 +0300

    Robot Init Pose + Dragging

--- src/Robot.py
+++ src/Robot.py
@@ -4,7 +4,10 @@ import pygame
 
 
 class Robot:
-    def __init__(self, constants_filename, preview_image_filename):
+    def __init__(self, initPose, constants_filename, preview_image_filename):
+        self.x, self.y, self.heading = initPose
+        self.effected_position = (0, 0)  # Preview Position (changes due to rotation)
+
         self.constants_filename = constants_filename
         self.load_constants()
 
@@ -20,9 +23,20 @@ class Robot:
             )
         )
 
-        self.preview = pygame.image.fromstring(
+        ortho_image = pygame.image.fromstring(
             resized.tobytes(), resized.size, resized.mode
         )
+        rotated_image = pygame.transform.rotate(ortho_image, self.heading)
+
+        original_size = ortho_image.get_size()
+        rotated_size = rotated_image.get_size()
+
+        self.rotated_position = (
+            self.x + (original_size[0] - rotated_size[0]) // 2,
+            self.y + (original_size[1] - rotated_size[1]) // 2,
+        )
+
+        self.preview = rotated_image.copy()
 
     def load_constants(self):
         with open(self.constants_filename, "r") as file:
@@ -33,3 +47,30 @@ class Robot:
 
     def get_preview(self):
         return self.preview
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_heading(self, heading):
+        self.heading = heading
+        self.generate_preview()
+
+    def get_position(self):
+        return (self.x, self.y)
+
+    def get_rotated_position(self):
+        return self.rotated_position
+
+    def get_heading(self):
+        return self.heading
+
+    def get_pose(self):
+        return (self.x, self.y, self.heading)
+
+    def cursor_over(self, cursor_position):
+        x, y = cursor_position
+        rx, ry = self.rotated_position
+        width, height = self.preview.get_size()
+
+        return rx <= x <= rx + width and ry <= y <= ry + height
